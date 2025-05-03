651ee10b39c3ae216a1ed61f0d4428a0f8629e37
31150 <3115089759@qq.com>
2025-05-04 00:04:25 +0800

    1

---
+++ src/test/java/com/lx/edusphere_server/EduSphereServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.edusphere_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSphereServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
